a2b8d8613a498bfe7d3b480787eaacc7ce63bb6b
Ludwig <ludovico.scarton@gmail.com>
2024-07-22 17:46:21 +0200

    adjusting template

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -1,24 +1,9 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
 
-setup(
-    name="{{ cookiecutter.project_slug }}",
-    version="{{ cookiecutter.version }}",
-    author="{{ cookiecutter.author_name }}",
-    author_email="{{ cookiecutter.email }}",
-    description="{{ cookiecutter.description }}",
-    license="{{ cookiecutter.license }}",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    install_requires=[
-        "rospy",
-        "std_msgs",
-    ],
-    extras_require={
-        "dev": [
-            "pytest",
-            "black",
-            "isort",
-            "flake8",
-        ],
-    },
+setup_args = generate_distutils_setup(
+  packages=["{{ cookiecutter.project_slug }}"],
+	package_dir={'': 'source'}
 )
+
+setup(**setup_args)
