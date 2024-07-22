9a0c1d5ca44c6f28a92cf436d8d87fb5141ec563
Ludwig <ludovico.scarton@gmail.com>
2024-07-22 22:53:54 +0200

    trying to compile and run

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -2,8 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-  packages=["{{ cookiecutter.project_slug }}"],
-	package_dir={'': 'source'}
+    packages=["{{ cookiecutter.project_slug }}"], package_dir={"": "source"}
 )
 
 setup(**setup_args)
--- {{cookiecutter.project_slug}}/test/test_{{ cookiecutter.project_slug }}.py
+++ {{cookiecutter.project_slug}}/test/test_{{ cookiecutter.project_slug }}.py
@@ -1,8 +1,10 @@
 import unittest
 
+
 class TestMyClass(unittest.TestCase):
     def test_case_1(self):
         self.assertEqual(1, 1)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
