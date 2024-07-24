9f53adbf51af430ef95d995f3340d38114c33650
Ludwig <ludovico.scarton@gmail.com>
2024-07-24 22:23:50 +0200

    fixing naming in package_dir

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages=["{{ cookiecutter.project_slug }}"], package_dir={"": "source"}
+    packages=['{{ cookiecutter.project_slug }}'], package_dir={'{{ cookiecutter.project_slug }}': 'src/{{ cookiecutter.project_slug }}'}
 )
 
 setup(**setup_args)
